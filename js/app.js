df5d212785aa522c386048e760dba0a2b0f8bb8b
kuani <116524310+kuani50@users.noreply.github.com>
2024-09-23 14:38:01 +0200

    :construction: Switch de temps de travail et ajout du bouton start

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+let start = false;
+let isWork = true;
+
+const workTime = 25;
+const pauseTime = 5;
+
+
+document.getElementById("play-btn").addEventListener("click",startBtn);
+
+
 function grayTheme(){
     replaceAllClass("red-bg","gray-bg");
     replaceAllClass("progress-circle-red","progress-circle-gray");
@@ -10,6 +20,14 @@ function redTheme(){
     replaceAllClass("neumorphism-gray","neumorphism-red");
 }
 
+function toggleNavBar(){
+    const elements = Array.from(document.getElementById("statusbar").children);
+    for(const i in elements) {
+        const element = elements[i];
+        console.log(element);
+        element.classList.toggle('unselected-status');
+    }
+}
 
 
 function replaceAllClass(oldClass,newClass){
@@ -22,26 +40,71 @@ function replaceAllClass(oldClass,newClass){
 }
 
 
+function updateProgessBar(initTime,minutes,secondes){
+    let time = minutes+(secondes/60);
+    let pourcent = ((1-(time/initTime))*100).toFixed(2);
+    updateCurrentProgressBar(pourcent)
+}
+
+
+function updateCurrentProgressBar(progress){
+    let elements = document.getElementsByClassName("progress-circle-red");
+    if(elements.length == 1){
+        elements[0].style.background=`radial-gradient(closest-side, #a10000 94%, transparent 80% 100%),conic-gradient(#22222280 ${progress}%,  wheat 0)`;
+        return;
+    }
+    elements = document.getElementsByClassName("progress-circle-gray");
+    if(elements.length == 1){
+        elements[0].style.background=`radial-gradient(closest-side, #434343 94%, transparent 80% 100%),conic-gradient(#22222280 ${progress}%,  wheat 0)`;
+        return;
+    }
+    console.error("Erreur progress bar");
+}
 
 function displayTime(minutes, secondes){
     document.getElementById("timer").innerText=String(minutes).padStart(2,'0') + ":" + String(secondes).padStart(2,'0');
 }
 
 
-
-
 function startChrono(initTime){
     let minutes = initTime;
     let secondes = 0
-    setInterval(() => {
+    let id = setInterval(() => {
         secondes--;
         if(secondes < 0){
             secondes=59
             minutes--;
         }
-        updateProgessBar(minutes,secondes);
+
+        if(minutes < 0){
+            clearInterval(id);
+            switchTime();
+            return;
+        }
+
+        updateProgessBar(initTime,minutes,secondes);
         displayTime(minutes,secondes);
     },1000)
 }
 
-startChrono(25);
+
+function switchTime(){
+    toggleNavBar();
+    if(isWork){
+        isWork=false;
+        grayTheme();
+        startChrono(pauseTime);
+    }else{
+        isWork=true;
+        redTheme();
+        startChrono(workTime);
+    }
+}
+
+
+function startBtn(){
+    if(!start){
+        start=true;
+        startChrono(workTime);
+    }
+}
